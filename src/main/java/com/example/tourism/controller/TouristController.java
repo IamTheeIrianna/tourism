fac55654b40ced1cd5375149023f6e8b92ad9289
XanderKjoller <xander@kjollers.dk>
2025-02-14 16:07:49 +0100

    klm

--- src/main/java/com/example/tourism/controller/TouristController.java
+++ src/main/java/com/example/tourism/controller/TouristController.java
@@ -6,7 +6,6 @@ import com.example.tourism.service.TouristService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/attractions")
@@ -26,17 +26,18 @@ public class TouristController {
         service = new TouristService(new TouristRepository());
     }
 
+
     @GetMapping("")
-    public ResponseEntity<List<TouristAttraction>> getAll()
+    public ResponseEntity<List<TouristAttraction>> getOne(@RequestParam(value = "attName", defaultValue = "_") String name)
     {
+        if (!Objects.equals(name, "_")) {
+            List<TouristAttraction> e = new ArrayList<>();
+            e.add(service.getTourAttractionsName(name));
+            return new ResponseEntity<>(e, HttpStatus.OK);
+        }
         List<TouristAttraction> t = service.getAllTouristAttractionsList();
         return new ResponseEntity<>(t, HttpStatus.OK);
-    }
 
-    @GetMapping("/")
-    public ResponseEntity<TouristAttraction> getOne(@RequestParam(value = "attName", defaultValue = "tivoli") String name)
-    {
-        return new ResponseEntity<>(service.getTourAttractionsName(name), HttpStatus.OK);
     }
 
     @PostMapping("/add")

7c88d801843acea6e30b268f4243287355396061
IamTheeIrianna <irpo0001@stud.kea.dk>
2025-02-14 15:56:08 +0100

    weblinks/paths

--- src/main/java/com/example/tourism/controller/TouristController.java
+++ src/main/java/com/example/tourism/controller/TouristController.java
@@ -5,55 +5,53 @@ import com.example.tourism.service.TouristService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/attractions")
 public class TouristController {
 
-    public TouristService service;
+    private final TouristService touristService;
 
-    public TouristController()
-    {
-        service = new TouristService();
+    public TouristController(TouristService touristService) {
+        this.touristService = touristService;
     }
-
+    //-------------------------" "-------------------------------
     @GetMapping("")
-    public ResponseEntity<ArrayList<TouristAttraction>> getAll()
-    {
-        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+    public ResponseEntity<List<TouristAttraction>> getAllTouristAttractionsList() {
+        List<TouristAttraction>touristAttractions = touristService.getAllTouristAttractionsList();
+        return new ResponseEntity<>(touristAttractions, HttpStatus.OK);
     }
-
-    @GetMapping("/search")
-    public ResponseEntity<TouristAttraction> getOne(@RequestParam(value = "attName", defaultValue = "tivoli") String name)
+    //----------------------------SEARCH by name----------------------------
+    @GetMapping("/{name}")
+    public ResponseEntity<TouristAttraction> getTourName(@RequestParam(value = "attName", defaultValue = "tivoli") String name)
     {
-        return new ResponseEntity<>(service.getTourAttractionsName(name), HttpStatus.OK);
+        return new ResponseEntity<>(touristService.findTourAttractionName(name), HttpStatus.OK);
     }
-
+    //------------------------ADD--------------------------------
     @PostMapping("/add")
     public ResponseEntity<TouristAttraction> addAttraction(@RequestParam(value = "attName", defaultValue = "tivoli") String name,@RequestParam(value = "attDesc", defaultValue = "tom") String desc)
     {
         TouristAttraction t = new TouristAttraction(name, desc);
-        return new ResponseEntity<>(service.addNewAttraction(t), HttpStatus.OK);
+        return new ResponseEntity<>(touristService.addNewAttraction(t), HttpStatus.OK);
     }
-
+    //-------------------------DELETE-------------------------------
     @PostMapping("/delete")
     public ResponseEntity<TouristAttraction> deleteAttraction(@RequestParam(value = "attName", defaultValue = "tivoli") String name)
     {
-        return new ResponseEntity(service.removeTourAttraction(name), HttpStatus.OK);
+        return new ResponseEntity(touristService.removeTourAttraction(name), HttpStatus.OK);
     }
-
+    //---------------------------UPDATE-----------------------------
     @PostMapping("/update")
     public ResponseEntity<TouristAttraction> updateAttraction(@RequestParam(value = "attName", defaultValue = "tivoli") String name,@RequestParam(value = "attDesc", defaultValue = "tom") String desc)
     {
         TouristAttraction t = new TouristAttraction(name, desc);
-        return new ResponseEntity<>(service.updateTourAttraction(t), HttpStatus.OK);
+        return new ResponseEntity<>(touristService.updateTourAttraction(t), HttpStatus.OK);
     }
 
 

f29b3c9699a0f8edc8d33d824a0a331f99471cca
XanderKjoller <xander@kjollers.dk>
2025-03-21 16:20:25 +0100

    sned help

--- src/main/java/com/example/tourism/TouristGuideAPI.java
+++ src/main/java/com/example/tourism/TouristGuideAPI.java
@@ -7,9 +7,8 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 public class  TouristGuideAPI {
 
+
     public static void main(String[] args) {
-        String test = System.getenv("PROD_DATABASE_URL");
-        System.out.println(test);
         SpringApplication.run(TouristGuideAPI.class, args);
     }
 
